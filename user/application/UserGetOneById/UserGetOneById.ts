248ff1c960c73e45cdd19cda4343cf1665862820
Aurelio <aurelio.mont25@gmail>
2024-08-28 14:51:21 -0600

    Application: Use Cases

---
+++ user/application/UserGetOneById/UserGetOneById.ts
@@ -0,0 +1,17 @@
+import { User } from "../../domain/User";
+import { UserId } from "../../domain/UserId";
+import { UserNotFoundError } from "../../domain/UserNotFoundError";
+import { UserRepository } from "../../domain/UserRepository";
+
+export class UserGetOneById {
+
+    constructor(private userRepository: UserRepository) { }
+    
+    async run(id: string): Promise<User> {
+        const user = await this.userRepository.getOneById(new UserId(id));
+
+        if (!user) throw new UserNotFoundError('User not found');
+        
+        return user;
+    }
+}
--- user/domain/UserRepository.ts
+++ user/domain/UserRepository.ts
@@ -5,6 +5,6 @@ export interface UserRepository {
     create(user: User): Promise<void>;
     getAll(): Promise<User[]>;
     getOneById(id: UserId): Promise<User | null>;
-    ediot(user: User): Promise<void>;
+    edit(user: User): Promise<void>;
     delete(id: UserId): Promise<void>;
 }
